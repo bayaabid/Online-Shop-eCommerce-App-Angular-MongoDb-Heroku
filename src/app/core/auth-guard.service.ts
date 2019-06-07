5c7c67f16b1337940205058bffebed9a43fa754c
unknown <roopkt@gmail.com>
2019-06-07 07:58:06 -0400

    added authguard

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -15,7 +15,7 @@ export class AuthGuardService implements CanActivate {
       return true;
     }
 
-    this.router.navigate(['login']);
+    this.router.navigate(['auth']);
     return false;
   }
 }
