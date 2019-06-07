35a5578f8924e80247abfbf8c503f9f59127a2aa
unknown <roopkt@gmail.com>
2019-06-07 08:28:57 -0400

    solved data table slow render

--- src/app/products/product-data.service.ts
+++ src/app/products/product-data.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { LogService } from '@core/logger.service';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class ProductDataService {
-  constructor(private $http: HttpClient) {}
+  constructor(private $http: HttpClient, private logService: LogService) {}
 
   getAllProducts() {
-    return this.$http.get('products.json');
+    this.logService.log('ProductDataService: Fetching Products from server');
+    return this.$http.get('products.json').pipe(delay(1000));
   }
 }
--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -9,19 +9,24 @@ import {
 import { MatTableDataSource } from '@angular/material/table';
 import { Product } from '@core/models/product';
 import { MatSort } from '@angular/material/sort';
+import { LogService } from '@core/logger.service';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
-  dataSource: MatTableDataSource<Product>;
+export class ProductsComponent implements OnInit {
+  dataSource = new MatTableDataSource<Product>();
+  loading = true;
   subscriptions = [];
   displayedColumns = ['imgUrl', 'name', 'price', 'addToCart'];
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private productDataService: ProductDataService) {}
+  constructor(
+    private productDataService: ProductDataService,
+    private logService: LogService
+  ) {}
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -29,14 +34,20 @@ export class ProductsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.subscriptions.push(
-      this.productDataService.getAllProducts().subscribe((products: any) => {
-        this.dataSource = new MatTableDataSource(products);
-        this.dataSource.sort = this.sort;
+      this.productDataService.getAllProducts().subscribe(products => {
+        this.logService.log('products loaded', products);
+        this.onDataLoad(products);
       })
     );
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(s => s.unsubscribe());
+  }
+
+  onDataLoad(products: any) {
+    this.loading = false;
+    this.dataSource.sort = this.sort;
+    this.dataSource.data = products;
   }
 }
